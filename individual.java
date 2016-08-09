4db338144e22ff6c1f63ff3a6f6f540b20e9573c
Raghavacherukuri <ragchintu@gmail.com>
2016-08-08 22:08:25 -0400

    Sample file

---
+++ individual.java
@@ -0,0 +1,16 @@
+package raghava;
+class individual
+{
+private int pr= 1;
+int def=2;
+public int p=3;
+protected int pt=4;
+individual()
+{
+System.out.println("private value is:"+pr);
+System.out.println("default value is:"+def);
+System.out.println("public value is:"+p);
+System.out.println("protected value is:"+pt);
+}
+}
+ 
